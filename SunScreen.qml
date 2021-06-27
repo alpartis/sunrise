5dc8fce00f5f13c7975c5a02ae926227a5dc505d
alan partis <alpartis@thundernet.com>
2021-06-26 23:29:56 -0400

    adjust to /forms resources in sunnygui

--- SunScreen.qml
+++ SunScreen.qml
@@ -1,5 +1,6 @@
 import QtQuick 2.12
 import sunnygui 1.0
+import forms 1.0
 
 Item {
     SunScreenForm {}
--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     QString path = QGuiApplication::applicationDirPath();
-    engine.addImportPath("qrc:/forms");
+    engine.addImportPath("qrc:/");
     engine.addImportPath("qrc:/imports");
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
